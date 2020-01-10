697f3dc6b67ded8920157d0dcef5e472803c8b2b
Rustamjon <abduvokhidov.rb@phystech.edu>
2020-01-10 14:46:01 +0500

    fix: fixes #2

--- main.go
+++ main.go
@@ -5,20 +5,20 @@ func main() {
 }
 
 func bonusCounter(sales []int) int {
-	const percentOfCashback = 5
+	const percentOfBonus = 5
 	const borderOfBonus = 10_000
 	const hundredPercent = 100
-	sumOfCashback := 0
+	sumOfBonus := 0
 	for _, sale := range sales {
 
 		if sale > borderOfBonus {
-			bonus := sale - borderOfBonus
-			cashback := percentOfCashback * bonus / hundredPercent
-			sumOfCashback += cashback
+			bonusFrom := sale - borderOfBonus
+			bonus := percentOfBonus * bonusFrom / hundredPercent
+			sumOfBonus += bonus
 		}
 
 	}
 
-	return sumOfCashback
+	return sumOfBonus
 
 }
