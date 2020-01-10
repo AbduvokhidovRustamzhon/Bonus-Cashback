fbfa9bfd7a024b3364c793d86569e47fb75f149d
Rustamjon <abduvokhidov.rb@phystech.edu>
2020-01-10 14:48:54 +0500

    fix: fixes #3

--- main_test.go
+++ main_test.go
@@ -9,7 +9,8 @@ func Test_bonusCounter(t *testing.T) {
 		sales []int
 		want int
 	}{
-		{name:"Cashback",sales:[]int{12_000, 15_000, 8_000, 9_000}, want:350},
+		{name:"Bonus",sales:[]int{12_000, 15_000, 8_000, 9_000}, want:350},
+		{name:"no Bonus", sales:[]int{3_000, 7_000, 8_000, 9_000}, want:0},
 	}
 	for _, test := range tests {
 		got := bonusCounter(test.sales)
